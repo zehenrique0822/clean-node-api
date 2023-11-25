faeecfbd73ce9982684dbf5b594b6aeae3b3c681
zehenrique0822 <zehenrique0822@gmail.com>
2023-11-25 00:38:35 -0300

    refactor: create mapping in mongohelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -13,5 +13,9 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map: (collection: any, result: any): any => {
+    return { id: result.insertedId.toString(), ...collection }
   }
 }
