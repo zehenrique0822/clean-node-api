1dad03081c35fb32c3ce78353452901608bd6e35
zehenrique0822 <zehenrique0822@gmail.com>
2024-01-01 17:02:33 -0300

    feat: create adaptRoute for controller

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
