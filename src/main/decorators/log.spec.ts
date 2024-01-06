e2489e0b8b06a8cae605afcb423ef191fb8bb9c1
zehenrique0822 <zehenrique0822@gmail.com>
2024-01-06 18:03:47 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -46,4 +46,23 @@ describe('LogControllerDecorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut, controllerStub } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'John Doe'
+      }
+    })
+  })
 })
